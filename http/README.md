ec5b9985ef2418b34e9ab22f6c9ae77924bec828
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-20 08:40:17 +0700

    using module chi-chi-go

--- http/README.md
+++ http/README.md
@@ -2,5 +2,6 @@
 # Module
 ```
 $ go get github.com/bmizerany/pat
+$ go get -u github.com/go-chi/chi/v5
 ```
 
--- http/cmd/web/routes.go
+++ http/cmd/web/routes.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/Nguyenthaiduc/go-web/pkg/config"
 	"github.com/Nguyenthaiduc/go-web/pkg/handlers"
-	"github.com/bmizerany/pat.com"
-	"net/http"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
-func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New() //mux mean multiplexer?
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+func routes(app *config.AppConfig) http.Handler {
+	// mux := pat.New() //mux mean multiplexer?
+
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+
+	//middleware
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
