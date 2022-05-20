49d3a7921fa94e65b691de2d4379b1f94ab755f7
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-05-20 09:39:18 +0700

    Experimenting with sessions

--- http/pkg/handlers/handlers.go
+++ http/pkg/handlers/handlers.go
@@ -32,6 +32,9 @@ func NewHandler(r *Repository) {
 
 // Home is the about page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(),"remote_ip",remoteIP) 
+
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
@@ -39,7 +42,13 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	//perform some logic
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello,again"
+
+	remoteIP := m.App.Session.GetString(r.Context(),"remote_ip")
+
 	
+
+	stringMap["remote_ip"] = remoteIP
+
 
 	//send
 
